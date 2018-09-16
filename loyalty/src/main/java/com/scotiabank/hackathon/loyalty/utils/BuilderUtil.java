4b115d4878326ea24a1918104c195ad5e190a14d
Camilo Varela <cvarela@grupodot.com>
2018-09-16 11:57:37 -0500

    Adding  Invision

--- loyalty/src/main/java/com/scotiabank/hackathon/loyalty/utils/BuilderUtil.java
+++ loyalty/src/main/java/com/scotiabank/hackathon/loyalty/utils/BuilderUtil.java
@@ -17,11 +17,21 @@ public class BuilderUtil {
         .redCoins(7000L).build());
     initialData.put(2L, CustomerResponse.builder().id(2L).fullName("Camilo Varela").email("ing.camilovarela@gmail.com")
         .redCoins(4500L).build());
-    initialData.put(3L, CustomerResponse.builder().id(3L).fullName("").email("")
+    initialData.put(3L, CustomerResponse.builder().id(3L).fullName("Marcelino Herrera").email("herrermr@colpatria.com")
         .redCoins(3000L).build());
-    initialData.put(4L, CustomerResponse.builder().id(4L).fullName("").email("")
+    initialData.put(4L, CustomerResponse.builder().id(4L).fullName("Oswaldo Álvarez").email("alvareos@colpatria.com")
         .redCoins(4000L).build());
-    initialData.put(5L, CustomerResponse.builder().id(5L).fullName("").email("")
+    initialData.put(5L, CustomerResponse.builder().id(5L).fullName("Juliana Adib").email("adibh@colpatria.com")
+        .redCoins(5000L).build());
+    initialData.put(6L, CustomerResponse.builder().id(6L).fullName("Andrés Arévalo").email("arevalca@colpatria.com")
+        .redCoins(6000L).build());
+    initialData.put(7L, CustomerResponse.builder().id(7L).fullName("Paola Vásquez").email("vasquep@colpatria.com")
+        .redCoins(5000L).build());
+    initialData.put(8L, CustomerResponse.builder().id(8L).fullName("Camilo Jiménez").email("camilo@mercadoni.com")
+        .redCoins(5000L).build());
+    initialData.put(9L, CustomerResponse.builder().id(9L).fullName("Libardo Lara").email("llara@co.ibm.com")
+        .redCoins(5000L).build());
+    initialData.put(10L, CustomerResponse.builder().id(10L).fullName("Sergio V").email("sergio@co.ibm.com")
         .redCoins(5000L).build());
     return initialData;
   }
